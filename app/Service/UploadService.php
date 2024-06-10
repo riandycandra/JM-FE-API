281f213ce37417b6b516108d91f80c1404db70d7
Riandy Candra Winahyu <riandycandra.dev@gmail.com>
2024-06-10 10:33:26 +0700

    fix(upload-service): fix upload move from gcp to local

--- app/Service/UploadService.php
+++ app/Service/UploadService.php
@@ -10,27 +10,10 @@ class UploadService
 {
     public function uploadFile(UploadedFile $file) {
 
-        $file->move(public_path('temp'), $file->getClientOriginalName());
-
-        $storage = new StorageClient([
-            'keyFilePath' => base_path() . '/service_account.json',
-        ]);
-        $bucketName = env('GOOGLE_CLOUD_STORAGE_BUCKET');
-        $bucket = $storage->bucket($bucketName);
-
-        $filePath = public_path('temp/' . $file->getClientOriginalName());
-
         $rand = Uuid::uuid4() . "." . $file->getClientOriginalExtension();
-        $targetDir = 'candidate-files/' . $rand;
 
-        $object = $bucket->upload(
-            fopen($filePath, 'r'), [
-                'name' => $targetDir
-            ]
-        );
+        $file->move(public_path('temp'), $rand);
 
-        unlink($filePath);
-
-        return "https://storage.googleapis.com/exam-fe-jasamarga/candidate-files/{$rand}";
+        return url('temp/' . $rand);
     }
 }
